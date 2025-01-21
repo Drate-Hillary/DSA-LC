c90725a0b00a0333903f4d07a13bb49cf3ba43ce
Drate <161850362+Drate-Hillary@users.noreply.github.com>
2025-01-21 22:33:07 +0300

    Time: 5 ms (81.90%) | Memory: 56.3 MB (50.26%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.js
+++ 12-integer-to-roman/integer-to-roman.js
@@ -4,27 +4,28 @@
  */
 var intToRoman = function(num) {
     const romanMap = [
-        { value: 1000, symbol: 'M' },
-        { value: 900, symbol: 'CM' },
-        { value: 500, symbol: 'D' },
-        { value: 400, symbol: 'CD' },
-        { value: 100, symbol: 'C' },
-        { value: 90, symbol: 'XC' },
-        { value: 50, symbol: 'L' },
-        { value: 40, symbol: 'XL' },
-        { value: 10, symbol: 'X' },
-        { value: 9, symbol: 'IX' },
-        { value: 5, symbol: 'V' },
-        { value: 4, symbol: 'IV' },
-        { value: 1, symbol: 'I' }
+        {value : 1000, symbol : 'M'},
+        {value : 900, symbol : 'CM'},
+        {value : 500, symbol : 'D'},
+        {value : 400, symbol : 'CD'},
+        {value : 100, symbol : 'C'},
+        {value : 90, symbol : 'XC'},
+        {value : 50, symbol : 'L'},
+        {value : 40, symbol : 'XL'},
+        {value : 10, symbol : 'X'},
+        {value : 9, symbol : 'IX'},
+        {value : 5, symbol : 'V'},
+        {value : 4, symbol : 'IV'},
+        {value : 1, symbol : 'I'},
     ];
 
     let res = '';
-    for(const{value, symbol} of romanMap){
+    for(const {value, symbol} of romanMap){
         while(num >= value){
             res += symbol;
-            num -= value
+            num -= value;
         }
     }
+
     return res;
 };
