0e725217071070bca397c69afa96802c95ec6865
Drate <161850362+Drate-Hillary@users.noreply.github.com>
2025-02-11 23:08:55 +0300

    Time: 1 ms (90.26%) | Memory: 51.9 MB (14.38%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,14 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = new Map();
+    let map = new Map();
 
     for(let i = 0; i < nums.length; i++){
-        const c = target - nums[i];
-        if(map.has(c)){
-            return [map.get(c), i];
+        let com = target - nums[i];
+
+        if(map.has(com)){
+            return [map.get(com), i];
         }
+
         map.set(nums[i], i);
     }
+
     return [];
 };
