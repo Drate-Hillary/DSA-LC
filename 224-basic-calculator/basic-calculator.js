e0a73a202498ec98281c90bd9431c9d023868bc2
Drate <161850362+Drate-Hillary@users.noreply.github.com>
2025-02-16 21:01:30 +0300

    Time: 24 ms (29.62%) | Memory: 54.4 MB (38.56%) - LeetSync

--- 224-basic-calculator/basic-calculator.js
+++ 224-basic-calculator/basic-calculator.js
@@ -5,33 +5,34 @@
 var calculate = function(s) {
     let stack = [];
     let num = 0;
-    let res = 0;
     let sign = 1;
+    let result = 0;
 
-    for(let i = 0; i < s.length; i++){
+    for (let i = 0; i < s.length; i++) {
         let char = s[i];
 
-        if(/\d/.test(char)){
-            num = num * 10 + parseInt(char, 10);
-        }else if(char === '+' || char === '-'){
-            res += sign * num;
+        if (char >= '0' && char <= '9') {
+            num = num * 10 + (char - '0');
+        } else if (char === '+') {
+            result += sign * num;
             num = 0;
-            sign = char === '+' ? 1 : -1;
-        }else if(char === '('){
-            stack.push(res);
-            stack.push(sign);
-
-            res = 0;
             sign = 1;
-        }else if(char === ')'){
-            res += sign * num;
+        } else if (char === '-') {
+            result += sign * num;
             num = 0;
-
-            res *= stack.pop();
-            res += stack.pop();
+            sign = -1;
+        } else if (char === '(') {
+            stack.push(result);
+            stack.push(sign);
+            result = 0;
+            sign = 1;
+        } else if (char === ')') {
+            result += sign * num;
+            num = 0;
+            result *= stack.pop();
+            result += stack.pop();
         }
     }
 
-    res += sign * num;
-    return res;
-};
+    return result + sign * num;
+}
