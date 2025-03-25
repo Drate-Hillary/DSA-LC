e557909978857332312bcd27d5f173bc057bab6f
Drate <161850362+Drate-Hillary@users.noreply.github.com>
2025-03-25 15:47:57 +0300

    Time: 0 ms (100.00%) | Memory: 55.4 MB (12.98%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+    
+    if(!root) return [];
+
+    const res = [];
+    const queue = [root];
+    let leftToRight = true;
+
+    while(queue.length > 0){
+        const size = queue.length;
+        const level = [];
+
+        for(let i = 0; i < size; i++){
+            const node = queue.shift();
+
+            if(leftToRight){
+                level.push(node.val);
+            }else{
+                level.unshift(node.val);
+            }
+
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+
+        res.push(level);
+        leftToRight = !leftToRight;
+    }
+
+    return res;
+};
